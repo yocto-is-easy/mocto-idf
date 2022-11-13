053df4ddd190e09a28c3094dd2c3d40bb8c4a917
Mykhailo Didur <mikhaildidur2003@gmail.com>
2022-11-13 18:30:03 +0200

    Add support of a callback
    Now it is possible to pass MIDF function as an argument to another
    MIDF function.
    
    Signed-off-by: Mykhailo Didur <mikhaildidur2003@gmail.com>

--- include/midf.hpp
+++ include/midf.hpp
@@ -5,6 +5,8 @@
 
 #include <lrrp.h>
 
+using json = nlohmann::json;
+
 template <typename ...Args>
 json parameter_pack_to_json(Args... args) {
     json ret = {args...};
@@ -46,10 +48,6 @@ private:
     virtual Ret process(Args... args) = 0;
 };
 
-#define MIDF_DECL_PORT(service_name, port) \
-    namespace service_name { \
-        const int midf_port = port; \
-    }
 
 #define LOCAL_HOST "127.0.0.1"
 
@@ -86,15 +84,79 @@ private:
     private: \
         virtual ret_t process(__VA_ARGS__) override; \
     }; \
-    int id_##name = service_name##_midf_server.add_handler(#name, [](const lrrp::request& req) -> lrrp::response { \
+    int id_##service_name##_##name = service_name##_midf_server.add_handler(#name, [](const lrrp::request& req) -> lrrp::response { \
         static handler_impl_##service_name##_##name name; \
         return name.handle(req); \
     }); \
     ret_t handler_impl_##service_name##_##name::process
 
-#define INIT_MIDF_SERVER(service_name) lrrp::server service_name##_midf_server(service_name::midf_port);
-#define START_MIDF_SERVER(service_name) service_name##_midf_server.run();
+#define MIDF_DECL_PORT(service_name, port) \
+    namespace service_name { \
+        const int midf_port = port; } \
+    MIDF_DECL_FUNC_NO_ARGS(bool, service_name, ping); \
 
-#define CALL_BACK
+#define INIT_MIDF_SERVER(service_name) \
+    lrrp::server service_name##_midf_server(service_name::midf_port); \
+    MIDF_IMPL_FUNC(bool, service_name, ping) () { return true; }
+
+#define START_MIDF_SERVER(service_name) \
+    supervisor::inform_about_me(AS_CALL_BACK(service_name, ping, bool)); \
+    service_name##_midf_server.run();
+
+#define START_MIDF_SERVER_WITHOUT_OBSERVER(service_name) \
+    service_name##_midf_server.run();
+
+namespace midf {
+
+    template <typename Ret, typename... Args>
+    class function {
+    public:
+        int m_port;
+        std::string m_service_name;
+        std::string m_func_name;
+
+    public:
+        function(int port, std::string service_name, std::string func_name)
+            : m_port(port)
+            , m_service_name(service_name)
+            , m_func_name(func_name) {}
+
+        function() = default;
+
+        Ret operator()(Args... args) {
+            lrrp::client cli(LOCAL_HOST, this->m_port);
+            auto req = lrrp::request_builder(m_func_name).set_json(parameter_pack_to_json<Args...>(args...)).build();
+            auto res = cli.send(req);
+            Ret ret;
+            res.get_payload().get_to(ret);
+            return ret;
+        }
+
+        std::string of_service() {
+            return m_service_name;
+        }
+
+    };
+
+    template <typename Ret, typename ...Args>
+    void to_json(nlohmann::json& j, const midf::function<Ret, Args...>& f) {
+        j = json {
+            {"port", f.m_port},
+            {"func_name", f.m_func_name},
+            {"service_name", f.m_service_name}
+        };
+    }
+
+    template <typename Ret, typename ...Args>
+    void from_json(const nlohmann::json& j, midf::function<Ret, Args...>& f) {
+        j.at("port").get_to(f.m_port);
+        j.at("func_name").get_to(f.m_func_name);
+        j.at("service_name").get_to(f.m_service_name);
+    }
+
+} // namespace midf
+
+#define AS_CALL_BACK(service_name, function_name, ...) \
+    midf::function<__VA_ARGS__>(service_name::midf_port, #service_name, #function_name)
 
 #endif
